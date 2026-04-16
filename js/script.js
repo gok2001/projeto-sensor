a34bdcf8ca7058d05b2a1b2aa939bfaee336c711
GUILHERME ORIGE KERNBICHLER <guilherme.kernbichler@senaisp.edu.br>
2026-04-15 21:42:30 -0300

    Adiciona js que pega os dados do banco e adiciona na tabela do dashboard

---
+++ js/script.js
@@ -0,0 +1,29 @@
+async function carregarTabela() {
+    try {
+        const response = await fetch('http://10.110.12.7:1880/getSensor');
+        const data = await response.json();
+        
+        atualizarTabela(data);
+    } catch (erro) {
+        console.error('Erro:', erro);
+    }
+}
+
+function atualizarTabela(data) {
+    const tbody = document.querySelector('#tabela-simples tbody');
+    tbody.innerHTML = '';
+
+    data.forEach(item => {
+        const linha = `
+            <tr>
+                <td>${item.temperatura}</td>
+                <td>${item.Umidade}</td>
+            </tr>
+        `;
+        tbody.innerHTML += linha;
+    });
+}
+
+// setInterval(atualizarTabela, 5000);
+
+carregarTabela();
